f9bce1602f5a798c7b90c989b7fba87f64435f69
ValentinePumpkins <quignon.val@gmail.com>
2024-01-22 11:57:24 +0100

    correct semistandard

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -8,10 +8,10 @@ request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(`error: ${error}`);
   } else {
-      const moviesData = JSON.parse(body);
-      const wedgeAntillesMovies = moviesData.results.filter(movie =>
-        movie.characters.some(character => character.includes('18'))
-      );
-      console.log(wedgeAntillesMovies.length);
-    }
+    const moviesData = JSON.parse(body);
+    const wedgeAntillesMovies = moviesData.results.filter(movie =>
+      movie.characters.some(character => character.includes('18'))
+    );
+    console.log(wedgeAntillesMovies.length);
+  }
 });
