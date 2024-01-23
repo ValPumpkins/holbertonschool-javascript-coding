bbb4aaa53ffb99e487dace0c36a9b12c7e5f9517
ValentinePumpkins <quignon.val@gmail.com>
2024-01-23 10:52:47 +0100

    change logic

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -4,21 +4,21 @@ const request = require('request');
 const apiUrl = process.argv[2];
 
 request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(`Error: ${error}`);
-  } else {
+  if (!error) {
     const todoData = JSON.parse(body);
     const completedTasksByUser = {};
 
     todoData.forEach((todo) => {
-      if (todo.completed) {
-        if (completedTasksByUser[todo.userId]) {
-          completedTasksByUser[todo.userId]++;
-        } else {
-          completedTasksByUser[todo.userId] = 1;
-        }
+    if (todo.completed) {
+      if (completedTasksByUser[todo.userId]) {
+        completedTasksByUser[todo.userId]++;
+      } else {
+        completedTasksByUser[todo.userId] = 1;
       }
-    });
+    }
+    })
     console.log(completedTasksByUser);
+  } else {
+    console.error(error);
   }
 });
