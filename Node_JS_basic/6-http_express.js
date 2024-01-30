bba4a323ca2a1d6178f7d34a79c8e354cb88e5a0
ValentinePumpkins <quignon.val@gmail.com>
2024-01-30 13:49:03 +0100

    correct lint

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-	response.send('Hello Holberton School!');
+  response.send('Hello Holberton School!');
 });
 
 const port = 1245;
 app.listen(port, () => {
-	console.log(`Server is running : http://localhost:${port}/`);
+  console.log(`Server is running : http://localhost:${port}/`);
 });
 module.exports = app;
