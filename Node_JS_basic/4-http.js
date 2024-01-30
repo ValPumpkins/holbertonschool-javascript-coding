a4f5aaa6f24301a8d7d82950f0d73c682bcdef4a
ValentinePumpkins <quignon.val@gmail.com>
2024-01-30 10:43:39 +0100

    server w/ http module

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('Hello Holberton School!');
+});
+
+const port = 1245;
+
+app.listen(port, () => {
+  console.log(`Server is running : http://localhost:${ port }/`);
+});
+
+module.exports = app;
