5a6d57f348f2148c90c40507ec315a4805cae095
ValentinePumpkins <quignon.val@gmail.com>
2024-01-30 11:28:09 +0100

    http server (wip)

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((request, response) => {
+  if (request.url === '/') {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello Holberton School!');
+  } else if (request.url === '/students') {
+    countStudents(process.argv[2])
+      .then(() => {
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.end('This is the list of our students');
+      })
+      .catch((error) => {
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.end(`Cannot load the database: ${error.message}`);
+      });
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.end('404 Not Found');
+  }
+});
+
+const port = 1245;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}/`);
+});
+
+module.exports = app;
