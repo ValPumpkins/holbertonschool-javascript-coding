a0734912a554f8f3a0a0c0b824b6bc547670a922
ValentinePumpkins <quignon.val@gmail.com>
2024-01-31 10:04:55 +0100

    try another way

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -4,10 +4,12 @@ class StudentsController {
   static async getAllStudents(request, response) {
     readDatabase(process.argv[2] || './database.csv')
       .then((studentsData) => {
-        const responseText =
-          `This is the list of our students\n` +
-          `Number of students in CS: ${studentsData.CS.length}. List: ${studentsData.CS}\n` +
-          `Number of students in SWE: ${studentsData.SWE.length}. List: ${studentsData.SWE}\n`;
+        const csStudents = studentsData.CS.join(', ');
+        const sweStudents = studentsData.SWE.join(', ');
+
+        const responseText = 'This is the list of our students\n'
+          + `Number of students in CS: ${studentsData.CS.length}. List: ${csStudents}\n`
+          + `Number of students in SWE: ${studentsData.SWE.length}. List: ${sweStudents}\n`;
 
         response.status(200).send(responseText);
       })
@@ -18,18 +20,19 @@ class StudentsController {
   }
 
   static async getAllStudentsByMajor(request, response) {
-    const major = request.params.major;
-
-    if (major !== 'CS' && major !== 'SWE') {
-      response.status(500).send('Major parameter must be CS or SWE');
-      return;
-    }
-
     try {
+      const { major } = request.params;
+
+      if (major !== 'CS' && major !== 'SWE') {
+        response.status(500).send('Major parameter must be CS or SWE');
+        return;
+      }
+
       const studentsData = await readDatabase(process.argv[2] || './database.csv');
       const students = studentsData[major] || [];
       response.status(200).send(`List: ${students.join(', ')}`);
     } catch (error) {
+      console.error('Error:', error);
       response.status(500).send('Cannot load the database');
     }
   }
