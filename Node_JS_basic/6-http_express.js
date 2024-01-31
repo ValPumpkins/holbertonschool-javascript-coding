b98d353dbbb943f85dd597dc837f737cb3f6b2ff
ValentinePumpkins <quignon.val@gmail.com>
2024-01-31 18:15:00 +0100

    fix lint hook ?

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,13 +1,16 @@
 const express = require('express');
 
 const app = express();
-
-app.get('/', (request, response) => {
-  response.send('Hello Holberton School!');
-});
-
 const port = 1245;
-app.listen(port, () => {
-  console.log(`Server is running : http://localhost:${port}/`);
-});
+
 module.exports = app;
+
+if (require.main === module) {
+  app.get('/', (request, response) => {
+    response.send('Hello Holberton School!');
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running : http://localhost:${port}/`);
+  });
+}
