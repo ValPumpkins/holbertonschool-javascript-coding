b86ca48b777ad7577c6582885063c40676a5ef28
ValentinePumpkins <quignon.val@gmail.com>
2024-02-01 14:17:08 +0100

    try another way

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,31 +1,32 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
+const db = process.argv[2];
+
 const app = express();
-
-app.get('/', (request, response) => {
-  response.send('Hello Holberton School!');
-});
-
-app.get('/students', async (request, response) => {
-  try {
-    const studentsData = await countStudents('database.csv');
-
-    response.set('Content-Type', 'text/plain');
-    response.write('This is the list of our students\n');
-    response.write(`Number of students: ${studentsData.total}\n`);
-    response.write(`Number of students in CS: ${studentsData.cs.length}. List: ${studentsData.cs}\n`);
-    response.write(`Number of students in SWE: ${studentsData.swe.length}. List: ${studentsData.swe}`);
-  } catch (error) {
-    response.status(500).send(`Cannot load the database: ${error.message}`);
-  } finally {
-    response.end();
-  }
-});
-
 const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+  res.end();
+});
+app.get('/students', (req, res) => {
+  res.write('This is the list of our students\n');
+  countStudents(db)
+    .then((result) => {
+      res.write(`${result.totalNumber}\n`);
+      res.write(`${result.CS}\n`);
+      res.write(`${result.SWE}`);
+      res.end();
+    })
+    .catch(() => {
+      console.error('Cannot load the database');
+      res.write('Internal Server Error');
+      res.end();
+    });
+});
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
 
 module.exports = app;
