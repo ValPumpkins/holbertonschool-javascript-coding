e0c30153a18f0ba3eb9c0b7e3b3afeaeedc62cc7
ValentinePumpkins <quignon.val@gmail.com>
2024-02-01 13:34:57 +0100

    revert to old code

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const app = express();
-const port = 1245;
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
 });
 
-app.listen(port);
-
+const port = 1245;
+app.listen(port, () => {
+  console.log(`Server is running : http://localhost:${port}/`);
+});
 module.exports = app;
