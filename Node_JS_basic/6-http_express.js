8ddf749d79c313d261b4a209a3b530c60af31f90
ValentinePumpkins <quignon.val@gmail.com>
2024-02-01 13:33:15 +0100

    TEST

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -3,14 +3,10 @@ const express = require('express');
 const app = express();
 const port = 1245;
 
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port);
+
 module.exports = app;
-
-if (require.main === module) {
-  app.get('/', (request, response) => {
-    response.send('Hello Holberton School!');
-  });
-
-  app.listen(port, () => {
-    console.log(`Server is running : http://localhost:${port}/`);
-  });
-}
